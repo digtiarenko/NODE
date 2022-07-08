99ba799767e35928a2079dcb0a12ac47d9a2c26a
Valentyn Digtiarenko <baggins999@gmail.com>
2022-07-08 19:58:36 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+https://ibb.co/8xdM1TN https://ibb.co/z7TNb7H https://ibb.co/s37fyHX
+https://ibb.co/F5BWxft
---
+++ index.js
@@ -0,0 +1,47 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require('./contacts');
+
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const AllContacts = await listContacts();
+      console.table(AllContacts);
+      break;
+
+    case 'get':
+      const oneContact = await getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case 'add':
+      const contactToAdd = await addContact(name, email, phone);
+      console.log(contactToAdd);
+      break;
+
+    case 'remove':
+      const contactToDelete = await removeContact(id);
+      console.log(contactToDelete);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+};
+
+invokeAction(argv);
