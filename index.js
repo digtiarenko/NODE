17ff597005cf056d79ce07330dc2a6dd58de609c
Valentyn Digtiarenko <baggins999@gmail.com>
2022-07-11 13:18:12 +0300

    final

--- index.js
+++ index.js
@@ -44,4 +44,11 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   }
 };
 
-invokeAction(argv);
+const start = async argv => {
+  try {
+    await invokeAction(argv);
+  } catch (error) {
+    console.log(error);
+  }
+};
+start(argv);
